e4fb027d4f2c38e3ad0792ce85bfaadcd5ff41a5
Valeria <lisenko.lera.17@mail.ru>
2026-03-13 06:35:20 +0700

    lab6 simple C=α*A*B+β*C with simple test

---
+++ gemm.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+//s, d, c, z 
+// gemm ( transA, transB, m, n, k, alpha, A, ldA, B, ldB, beta, C, ldC ) 
+// C = α*A∗B∗ + βC 
+
+void gemm(int m, int n, int k, float alpha, float* A, float* B, float beta, float* C) {
+    // C = alpha*A*B + beta*C
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            float sum = 0;
+            for (int p=0; p<k; p++) {
+                sum += A[i*k+p] * B[p*n+j];
+            }
+            C[i*n+j] = alpha*sum + beta*C[i*n+j];
+        }
+    }
+}
+
+int main(){
+    //test
+    int m=3,n=2,k=1;
+    float A[3] = {1,2,3} ;
+    float B[2] = {1,2};
+    float C[6] = {0,0,0,0,0,0};
+    float a=1, b=1;
+
+    gemm(m, n, k, a, A, B, b, C);
+    
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            cout << C[i*n+j] << " ";
+        }
+        cout << "\n";
+    }
+}
