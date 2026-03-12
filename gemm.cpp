59ff703b48f632aa99c32701e2724d75f63766ac
Valeria <lisenko.lera.17@mail.ru>
2026-03-13 06:42:04 +0700

    lab6 add lda,ldb,ldc

--- gemm.cpp
+++ gemm.cpp
@@ -6,15 +6,15 @@ using namespace std;
 // gemm ( transA, transB, m, n, k, alpha, A, ldA, B, ldB, beta, C, ldC ) 
 // C = α*A∗B∗ + βC 
 
-void gemm(int m, int n, int k, float alpha, float* A, float* B, float beta, float* C) {
+void gemm(int m, int n, int k, float alpha, float* A, float* B, float beta, float* C, int lda, int ldb, int ldc) {
     // C = alpha*A*B + beta*C
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
             float sum = 0;
             for (int p=0; p<k; p++) {
-                sum += A[i*k+p] * B[p*n+j];
+                sum += A[i*lda+p] * B[p*ldb+j];
             }
-            C[i*n+j] = alpha*sum + beta*C[i*n+j];
+            C[i*ldc+j] = alpha*sum + beta*C[i*ldc+j];
         }
     }
 }
@@ -27,7 +27,7 @@ int main(){
     float C[6] = {0,0,0,0,0,0};
     float a=1, b=1;
 
-    gemm(m, n, k, a, A, B, b, C);
+    gemm(m, n, k, a, A, B, b, C, k, n, n);
     
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
