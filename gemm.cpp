a2a996d460c097e3d07e7df782f36271e198c93f
Valeria <lisenko.lera.17@mail.ru>
2026-03-13 07:05:57 +0700

    lab6 add transA, transB

--- gemm.cpp
+++ gemm.cpp
@@ -7,13 +7,29 @@ using namespace std;
 // C = α*A∗B∗ + βC 
 
 template<typename T>
-void gemm(int m, int n, int k, T alpha, T* A, T* B, T beta, T* C, int lda, int ldb, int ldc) {
-    // C = alpha*A*B + beta*C
+void gemm(int m, int n, int k, T alpha, T* A, T* B, T beta, T* C, int lda, int ldb, int ldc, char transA, char transB) {
+
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
             T sum = 0;
             for (int p=0; p<k; p++) {
-                sum += A[i*lda+p] * B[p*ldb+j];
+                T a, b;
+
+                if (transA == 'N') {
+                    a = A[i*lda+p];
+                } else {
+                    a = A[p*lda+i];
+                }
+                
+                
+                if (transB == 'N') {
+                    b = B[p*ldb+j];
+                } else {
+                    b = B[j*ldb+p];
+                }
+                
+                sum += a * b;
+
             }
             C[i*ldc+j] = alpha*sum + beta*C[i*ldc+j];
         }
@@ -30,7 +46,7 @@ int main(){
     double C[6] = {0,0,0,0,0,0};
     double a=1, b=1;
 
-    gemm(m, n, k, a, A, B, b, C, k, n, n);
+    gemm(m, n, k, a, A, B, b, C, k, n, n, 'N', 'N');
     
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
